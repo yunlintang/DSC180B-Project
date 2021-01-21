656f94702962f0a87163451cf8e417377e7c9e53
stevenzheng33 <stevenzheng33@163.com>
2021-01-20 21:26:34 -0800

    added daily sentiment py

---
+++ src/analysis/daily_sentiment.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+import datetime
+
+
+
+def gen_date_list(start_str, end_str):
+    """Generate the date list in string format of 
+    2020-01-01
+    used to read in all the csv documents
+    """
+    start = datetime.datetime.strptime(start_str, "%Y-%m-%d")
+    end = datetime.datetime.strptime(end_str, "%Y-%m-%d")
+    date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+    date_list = []
+    for date in date_generated:
+        date_list.append(date.strftime("%Y-%m-%d"))
+    return date_list
+
+def cal_daily_vader_score(file_path,date_list):
+    sid = SentimentIntensityAnalyzer()
+    pol = sid.polarity_scores
+    score_list = []
+    for i in date_list:
+        print(i)
+        test_df = pd.read_csv(file_path + i + '.csv')
+        score = test_df.text.apply(pol).apply(lambda x:x['compound']).mean()
+        score_list.append(score)
+    return score_list
